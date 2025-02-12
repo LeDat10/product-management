03d9b3c0b4d049d8e5001cdd263290434126df01
ledat <ledat1010204@gmail.com>
2025-02-12 20:22:03 +0700

    add category for product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,9 +1,11 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
+const createTreeHelper = require("../../helpers/createTree");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -131,9 +133,18 @@ module.exports.deleteItem = async (req, res) => {
 }
 
 // [GET] /admin/products/create
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+
+    let find = {
+        deleted: false,
+    };
+
+    const category = await ProductCategory.find(find);
+    const newCategory = createTreeHelper.tree(category);
+
     res.render("admin/pages/products/create", {
-        titlePage: "Thêm mới sản phẩm"
+        titlePage: "Thêm mới sản phẩm",
+        category: newCategory
     });
 }
 
@@ -163,11 +174,17 @@ module.exports.edit = async (req, res) => {
         };
 
         const product = await Product.findOne(find);
+    
+        const category = await ProductCategory.find({
+            deleted: false
+        });
+        const newCategory = createTreeHelper.tree(category);
 
         // console.log(product);
         res.render("admin/pages/products/edit", {
             titlePage: "Chỉnh sửa sản phẩm",
-            product: product
+            product: product,
+            category: newCategory
         });
     } catch (error) {
         res.redirect(`${systemConfig.prefixAdmin}/products`);

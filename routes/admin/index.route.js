d97a99633a4c4ad51f19eac9a1cc565b89045fd4
ledat <ledat1010204@gmail.com>
2025-02-13 20:03:57 +0700

    add role

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,10 +3,12 @@ const systemConfig = require('../../config/system');
 const dashboardRoutes = require('./dashboard.route');
 const productRoutes = require('./product.route');
 const productsCategoryRoutes = require("./products-category.route");
+const roleRoutes = require("./role.router");
 
 module.exports = (app) => {
     PATH_ADMIN = systemConfig.prefixAdmin;
     app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
     app.use(PATH_ADMIN + "/products", productRoutes);
     app.use(PATH_ADMIN + "/products-category", productsCategoryRoutes);
+    app.use(PATH_ADMIN + "/roles", roleRoutes);
 }

9e7b7062efa26d874a1bd244be023dfbabff8df7
ledat <ledat1010204@gmail.com>
2025-02-18 19:25:04 +0700

    Trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 
 const productHelper = require("../../helpers/product");
+const productCategoryHelper = require("../../helpers/product-category");
 
 
 // [GET] /products
@@ -8,7 +10,7 @@ module.exports.index = async (req, res) => {
     const products = await Product.find({
         status: "active",
         deleted: false
-    }).sort({position: "desc"});
+    }).sort({ position: "desc" });
 
     const newProduct = productHelper.pricenewProducts(products);
 
@@ -27,7 +29,7 @@ module.exports.detail = async (req, res) => {
             slug: req.params.slug,
             status: "active"
         }
-    
+
         const product = await Product.findOne(find);
         // console.log(product);
         res.render('client/pages/products/detail', {
@@ -38,3 +40,31 @@ module.exports.detail = async (req, res) => {
         res.redirect('/products');
     }
 };
+
+// [GET] /:slugCategory
+module.exports.category = async (req, res) => {
+    const slug = req.params.slugCategory;
+
+    const category = await ProductCategory.findOne({
+        slug: slug,
+        deleted: false,
+        status: "active"
+    });
+
+    const listSubCategory = await productCategoryHelper.getSubCategory(category.id);
+
+    const listSubCategoryId = listSubCategory.map(item => item.id);
+
+    const products = await Product.find({
+        product_category_id: { $in: [category.id, ...listSubCategoryId] },
+        deleted: false,
+        status: "active"
+    }).sort({ position: "desc" });
+
+    const newProducts = productHelper.pricenewProducts(products);
+
+    res.render('client/pages/products/index', {
+        pageTitle: category.title,
+        products: newProducts,
+    });
+}

675fb59d4dcadb2c9176c05a2556946443ca7cf2
ledat <ledat1010204@gmail.com>
2025-02-18 20:58:23 +0700

    trang chi tiết sản phẩm bên client

--- helpers/product.js
+++ helpers/product.js
@@ -6,3 +6,8 @@ module.exports.pricenewProducts = (products) => {
 
     return newProducts;
 }
+
+module.exports.pricenewProduct = (product) => {
+    const priceNew = (product.price * (100 - product.discountPercentage) / 100).toFixed(2);
+    return priceNew;
+}

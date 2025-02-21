f87bd41f9c82f65bdff9c4fc7ce4a86d97ba0dcb
ledat <ledat1010204@gmail.com>
2025-02-21 23:18:08 +0700

    lấy data gửi lên lưu vào database

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,5 +1,6 @@
 const Cart = require("../../models/carts.model");
 const Product = require("../../models/product.model");
+const Order = require("../../models/order.model");
 
 const productHelper = require("../../helpers/product");
 
@@ -34,3 +35,51 @@ module.exports.index = async (req, res) => {
         cartDetail: cart
     });
 };
+
+// [POST] /checkout/order
+module.exports.order = async(req, res) => {
+    const cartId = req.cookies.cartId;
+
+    const userInfo = req.body;
+
+    const cart  = await Cart.findOne({
+        _id: cartId
+    });
+
+    let products = [];
+
+    for (const product of cart.products) {
+        const objectProduct = {
+            product_id: product.product_id,
+            quantity: product.quantity,
+            price: 0,
+            discountPercentage: 0
+        };
+
+        const productInfo = await Product.findOne({
+            _id: product.product_id
+        });
+
+        objectProduct.price = productInfo.price;
+        objectProduct.discountPercentage = productInfo.discountPercentage;
+
+        products.push(objectProduct);
+    }
+
+    const objectOrder = {
+        cart_id: cartId,
+        userInfo: userInfo,
+        products: products
+    };
+
+    const order = new Order(objectOrder);
+    await order.save();
+
+    await Cart.updateOne({
+        _id: cartId
+    }, {
+        products: []
+    });
+
+    res.redirect(`/checkout/success/${order.id}`);
+}

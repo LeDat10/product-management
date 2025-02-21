cae546f9e0b6896e43e913a2d8159bf8f1bbf4b3
ledat <ledat1010204@gmail.com>
2025-02-21 21:28:26 +0700

    làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,39 @@
 const Cart = require("../../models/carts.model");
+const Product = require("../../models/product.model");
+
+const productHelper = require("../../helpers/product");
+
+// [GET] /cart
+module.exports.index = async (req, res) => {
+    const cartId =req.cookies.cartId;
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    });
+
+    if(cart.products.length > 0) {
+        for (const item of cart.products) {
+            const productId = item.product_id;
+            const productInfo = await Product.findOne({
+                _id: productId
+            });
+            productInfo.priceNew = productHelper.pricenewProduct(productInfo);
+        
+            item.productInfo = productInfo;
+
+            item.totalPrice =  item.quantity * productInfo.priceNew;
+        };
+    }
+
+    cart.totalPrice = cart.products.reduce((sum, item) => sum  = sum + item.totalPrice ,0);
+
+    res.render("client/pages/cart/index", {
+        titlePage: "Giỏ hàng",
+        cartDetail: cart
+    });
+};
+
+
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
     const cartId = req.cookies.cartId;
